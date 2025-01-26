918903a57f66c8110f6ebae42eaee8c6ad11c0bb
Dhruv Khatter <112469222+LearnWithDhruv@users.noreply.github.com>
2025-01-26 22:53:44 +0530

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,3 +7,12 @@ class GoogleCredentials(models.Model):
 
     def __str__(self):
         return self.user_email
+
+class CalendarEvent(models.Model):
+    title = models.CharField(max_length=255)  
+    location = models.CharField(max_length=255, blank=True, null=True)  
+    start_time = models.DateTimeField()  
+    created_at = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return self.title
